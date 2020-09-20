384909e1cb27c02bdc901a93d4294322db0ffc1c
SAMIASAADANI <45880788+THETIGERSEYES@users.noreply.github.com>
2020-09-20 02:52:42 +0200

    creation index.js
    exercice variable js

---
+++ index.js
@@ -0,0 +1,28 @@
+ /*
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+let userSurname = "David";
+let userLastname = "Bruno";
+let userMessage = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+let image1 = "http://www.go.com/davif.png";
+
+let userTown = "San Francisco"; 
+const ABBREVIATION_CALIFORNIA ="CA";
+ 
+let userFollowers = 109;
+let userFollowing = 1500;
+  
+const WHAT_TIME_IS  = new Date();
+
+  /*
+
+    🏁 David is following one more account, increment the total of following account
+
+  */
+peopleFollowing ++;
+console.log(peopleFollowing);
