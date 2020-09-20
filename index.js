b1176227d4b17c11f7acc2bdf0eb76ccbf92b55f
SAMIASAADANI <45880788+THETIGERSEYES@users.noreply.github.com>
2020-09-20 08:32:52 +0200

    Update index.js
    ok

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
- /*
+   /*
 
     🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
     variable keyword you have learned in the quest.
     ex: const name = "David";
 
   */
-let userSurname = "David";
-let userLastname = "Bruno";
+const userSurname = "David";
+const userLastname = "Bruno";
 let userMessage = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
 
 let image1 = "http://www.go.com/davif.png";
@@ -24,5 +24,5 @@ const WHAT_TIME_IS  = new Date();
     🏁 David is following one more account, increment the total of following account
 
   */
-peopleFollowing ++;
-console.log(peopleFollowing);
+userFollowing ++;
+console.log(userFollowing);
